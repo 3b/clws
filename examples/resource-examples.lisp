a79802021c3858deef72d25324cbbfb0f6fe9bb6
Bart Botta <00003b@gmail.com>
2011-09-17 09:45:00 -0500

    update examples

--- examples/resource-examples.lisp
+++ examples/resource-examples.lisp
@@ -26,6 +26,8 @@
 
 (defmethod resource-received-text ((res echo-resource) client message)
   #++(format t "got frame ~s from client ~s" message client)
+  (when (string= message "error")
+    (error "got \"error\" message "))
   (write-to-client-text client message))
 
 (defmethod resource-received-binary((res echo-resource) client message)
@@ -81,30 +83,51 @@
  #++
  (ws::origin-prefix "http://127.0.0.1" "http://localhost"))
 
-(defmethod resource-accept-connection ((res chat-server) resource-name headers client)
-  (declare (ignore headers resource-name))
+(defmethod resource-client-connected ((res chat-server) client)
   (format t "got connection on chat server from ~s : ~s~%" (client-host client) (client-port client))
-  ;; this gets called from wrong thread?
   (push client (clients res))
+  (let ((*print-pretty* nil))
+    (write-to-clients-text (clients res)
+                           (format nil "client joined from ~s.~s, ws protocol ~s"
+                                   (client-host client)
+                                   (client-port client)
+                                   (client-websocket-version client)))
+    (write-to-client-text client
+                          (format nil "headers = ~s~%"
+                                  (alexandria:hash-table-alist (client-connection-headers client)))))
   t)
 
 (defmethod resource-client-disconnected ((resource chat-server) client)
   (format t "Client disconnected from resource ~A: ~A~%" resource client)
-  (setf (clients resource) (remove client (clients resource))))
+  (setf (clients resource) (remove client (clients resource)))
+    (write-to-clients-text (clients resource)
+                           (format nil "client from ~s.~s left"
+                                   (client-host client)
+                                   (client-port client))))
 
 (defmethod resource-received-text ((res chat-server) client message)
   ;(format t "got frame ~s from chat client ~s" message client)
   (let ((*print-pretty* nil))
-    (write-to-clients (clients res)
-                      (concatenate '(vector (unsigned-byte 8))
-                                   '(0)
-                                   (babel:string-to-octets
-                                    (format nil "chat: ~s.~s : |~s|"
-                                            (client-host client)
-                                            (client-port client)
-                                            message)
-                                    :encoding :utf-8)
-                                   '(#xff)))))
+    (write-to-clients-text (clients res)
+                           (format nil "chat: ~s.~s : |~s|"
+                                   (client-host client)
+                                   1(client-port client)
+                                   message))))
+
+(defmethod resource-received-binary ((res chat-server) client message)
+  ;(format t "got frame ~s from chat client ~s" message client)
+  (let ((*print-pretty* nil)
+        (binary-clients)
+        (text-clients))
+    (loop for c in (clients res)
+          do (if (> (client-websocket-version c) 0)
+                 (push c binary-clients)
+                 (push c text-clients)))
+    (write-to-clients-text (clients res)
+                           (format nil "chat: binary message from ~s.~s"
+                                   (client-host client)
+                                   (client-port client)))
+    (write-to-clients-binary binary-clients message)))
 
 #++
 (bordeaux-threads:make-thread
